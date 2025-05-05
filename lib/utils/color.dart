6a01bbd34e69359ce2f6c56f50593b209a28edd7
Yumin Gui <guiyumin@protonmail.com>
2025-05-04 22:27:16 -0700

    feat: setup sidebar

---
+++ lib/utils/color.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class FinchChatColor {
+  static const Color primary = Color(0xFFF5F5DC);
+}
---
+++ lib/utils/theme.dart
@@ -0,0 +1,6 @@
+import 'package:finch_chat/utils/color.dart';
+import 'package:flutter/material.dart';
+
+ThemeData finchChatTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: FinchChatColor.primary),
+);
